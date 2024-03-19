8176442468e9c3d054f677eb8c3064303169a0ce
lovelyoyrmia <mokalulovelyo@gmail.com>
2024-03-19 14:59:48 +0700

    feat: added field violations errors

--- domain/services/movie.go
+++ domain/services/movie.go
@@ -1,9 +1,18 @@
 package services
 
 import (
+	"errors"
+	"fmt"
+
 	"github.com/lovelyoyrmia/movies-api/domain/pb"
 	"github.com/lovelyoyrmia/movies-api/internal/db"
+	"github.com/lovelyoyrmia/movies-api/pkg/validator"
+	"google.golang.org/genproto/googleapis/rpc/errdetails"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
+
+var ErrEmptyFields = errors.New("cannot be empty")
 
 type MovieService struct {
 	pb.UnimplementedMovieServicesServer
@@ -14,4 +23,33 @@ func NewMovieService(store db.Store) *MovieService {
 	return &MovieService{
 		store: store,
 	}
+}
+
+func validateMovieParamsRequest(req *pb.MovieParams) (violations []*errdetails.BadRequest_FieldViolation) {
+	if req.Title == "" {
+		err := fmt.Errorf("title %s", ErrEmptyFields.Error())
+		return append(violations, validator.FieldViolation("title", err))
+	}
+
+	if req.Description == "" {
+		err := fmt.Errorf("description %s", ErrEmptyFields.Error())
+		return append(violations, validator.FieldViolation("description", err))
+	}
+
+	if err := validator.ValidateTitle(req.GetTitle()); err != nil {
+		return append(violations, validator.FieldViolation("title", err))
+	}
+
+	return violations
+}
+
+func invalidArgumentError(violations []*errdetails.BadRequest_FieldViolation) error {
+	badReq := &errdetails.BadRequest{FieldViolations: violations}
+	statusInvalid := status.New(codes.InvalidArgument, "invalid request")
+
+	statusDetails, err := statusInvalid.WithDetails(badReq)
+	if err != nil {
+		return statusInvalid.Err()
+	}
+	return statusDetails.Err()
 }
---
+++ pkg/validator/field_violation.go
@@ -0,0 +1,10 @@
+package validator
+
+import "google.golang.org/genproto/googleapis/rpc/errdetails"
+
+func FieldViolation(field string, err error) *errdetails.BadRequest_FieldViolation {
+	return &errdetails.BadRequest_FieldViolation{
+		Field:       field,
+		Description: err.Error(),
+	}
+}
